7dcfe917adedd32a1d2e56bea7757c57ec78fd8a
Duarte Areias dos Reis <duarte.reis@criticaltechworks.com>
2022-11-09 12:35:03 +0000

    refactored

--- srcs/requirements/mariadb/tools/init_database.sh
+++ srcs/requirements/mariadb/tools/init_database.sh
@@ -1,45 +1,40 @@
 #!/bin/bash
-mysql_install_db;
-# Make sure mariadb is on
-service mysql start
+if [ ! -d "/var/lib/mysql/${DB_NAME}" ]; then
+    mysql_install_db;
+    # Make sure mariadb is on
+    service mysql start
 
-# Enforce password usage
-mysql -e "UPDATE mysql.user SET Password = PASSWORD('$DB_PASSWORD') WHERE User = 'root'"
+    # Enforce password usage
+    mysql -e "UPDATE mysql.user SET Password = PASSWORD('$DB_ROOT_PASSWORD') WHERE User = 'root'"
 
-# Drop anonymous users
-mysql -e "DROP USER IF EXISTS '@''localhost'"
-mysql -e "DROP USER IF EXISTS ''@'$(hostname)'"
+    # Drop anonymous users
+    mysql -e "DROP USER IF EXISTS '@''localhost'"
+    mysql -e "DROP USER IF EXISTS ''@'$(hostname)'"
 
-# Drop test database
-mysql -e "DROP DATABASE IF EXISTS test"
+    # Drop test database
+    mysql -e "DROP DATABASE IF EXISTS test"
 
-# Create wordpress database
-mysql -e "CREATE DATABASE IF NOT EXISTS $DB_NAME"
+    # Create wordpress database
+    mysql -e "CREATE DATABASE IF NOT EXISTS $DB_NAME"
 
-# Entering dummy tables and data
-mysql -e "use $DB_NAME;\
+    # Entering dummy tables and data
+    mysql -e "use $DB_NAME;\
     CREATE TABLE IF NOT EXISTS data \
     ( data_id INT AUTO_INCREMENT PRIMARY KEY, \
     title VARCHAR(255) NOT NULL );"
 
-mysql -e "use $DB_NAME;\
+    mysql -e "use $DB_NAME;\
     INSERT INTO data (title) VALUES('A');"
 
-mysql -e "use $DB_NAME;\
+    mysql -e "use $DB_NAME;\
     INSERT INTO data (title) VALUES('B');"
 
-# Creating wordpress user
-# mysql -e "CREATE USER IF NOT EXISTS '$DB_USER@$WORDPRESS_IP' IDENTIFIED BY '$DB_PASSWORD'"
-mysql -e "CREATE USER IF NOT EXISTS '$DB_USER' IDENTIFIED BY '$DB_PASSWORD'"
+    # Creating wordpress user
+    mysql -e "CREATE USER IF NOT EXISTS '$DB_USER' IDENTIFIED BY '$DB_PASSWORD'"
 
-# mysql -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER@$WORDPRESS_IP'"
-mysql -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'"
+    mysql -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'"
 
-mysql -e "FLUSH PRIVILEGES"
-
-# Check if database doesn't already exist
-if [[ ! -d "/var/lib/mysql" ]]; then
-    echo "Database does not exit"
+    mysql -e "FLUSH PRIVILEGES"
 fi
 
 mysqladmin -u root -p${DB_PASSWORD} shutdown
--- srcs/requirements/nginx/tools/nginx_conf_setup.sh
+++ srcs/requirements/nginx/tools/nginx_conf_setup.sh
@@ -2,7 +2,7 @@
 
 sed -i "s/REPLACEMEUSERLOGIN/$USER_LOGIN/g" /etc/nginx/sites-available/wordpress;
 sed -i "s/REPLACEMEWORDPRESSNAME/$WP_NAME/g" /etc/nginx/sites-available/wordpress;
-sed -i "s/REPLACEMEWORDPRESSIP/$WP_IP/g" /etc/nginx/sites-available/wordpress;
+sed -i "s/REPLACEMEWORDPRESSPORT/$WP_PORT/g" /etc/nginx/sites-available/wordpress;
 
 ln -sf /etc/nginx/sites-available/wordpress /etc/nginx/sites-enabled/wordpress;
 # unlink /etc/nginx/sites-enabled/default;
